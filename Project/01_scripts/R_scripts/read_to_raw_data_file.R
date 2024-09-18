738ec21a4fc5df90f1d740fd626b279d030f1f4e
meganduchesne <megan.duchesne13@gmail.com>
2024-09-18 11:24:31 -0700

    reading raw data to csv with more accurate name

--- Project/01_scripts/R_scripts/read_to_raw_data_file.R
+++ Project/01_scripts/R_scripts/read_to_raw_data_file.R
@@ -8,18 +8,9 @@ library(openxlsx)
 library(tidyverse)
 
 # read in data files
-file_morphology <- "mean_traits_merged_morphology.xlsx"  
+file_morphology <- "Project/00_Raw_data/standardized_traits_sep11.xlsx"  
 morphology <- read.xlsx(file_morphology)
-
-file_survival_before_2019 <- "survival_pirmin.xlsx"  
-survival_before_2019 <- read.xlsx(file_survival_before_2019)
-
-file_survival_after_2019 <- "2019-2023_survival_file.xlsx"  
-survival_after_2019 <- read.xlsx(file_survival_after_2019)
-
 
 
 # Write the data to a CSV in the rawdata folder
 write.csv(morphology, file = "Project/00_Raw_data/morphology.csv", row.names = FALSE)
-write.csv(survival_before_2019, file = "Project/00_Raw_data/survival_before_2019.csv", row.names = FALSE)
-write.csv(survival_after_2019, file = "Project/00_Raw_data/survival_after_2019.csv", row.names = FALSE)
